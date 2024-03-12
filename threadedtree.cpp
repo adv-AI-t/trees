8148b69b36ef0b579430e1fa2461601bcfb13643
adv-AI-t <joshiadvait07@gmail.com>
2024-03-12 09:04:50 +0530

    Update

--- threadedtree.cpp
+++ threadedtree.cpp
@@ -34,14 +34,86 @@ class Tree
         Node* root = new Node(data);
     }
 
-    void insertNode(int value)
+    void insert(int value)
     {
+        Node* ptr = root;
+        Node* parent = NULL;
+        while(ptr)
+        {
+            parent = ptr;
+            if(value<ptr->data)
+            {
+                if(!ptr->lthread)
+                ptr = ptr->left;
+                else
+                break;
+            }
+            else
+            {
+                if(!ptr->rthread)
+                ptr = ptr->right;
+                else
+                break;
+            }
+        }
+        Node* newnode = new Node(value);
+        if(!parent)
+        {
+            root = newnode;
+            return;
+        }
+        if(value<parent->data)
+        {
+            newnode->left = parent->left;
+            newnode->lthread = parent->lthread;
+            parent->lthread = false;
+            parent->left = newnode;
+            newnode->right = parent;
+            newnode->rthread = true;
+        }
+        else
+        {
+            newnode->right = parent->right;
+            newnode->rthread = parent->rthread;
+            parent->right = newnode;
+            parent->rthread = false;
+            newnode->left = parent;
+            newnode->lthread = true;
+        }
+    }
 
+    Node* leftmostNode(Node* root)
+    {
+        Node* temp = root;
+        while(temp->left&&!temp->lthread)
+        temp = temp->left;
+        return temp;
+    }
+
+    Node* inorder()
+    {
+        if(!root->right)
+        return NULL;
+        Node* temp = root;
+        temp = leftmostNode(temp);
+        while(temp)
+        {
+            cout<<temp->data<<" ";
+            if(temp->rthread)
+            temp = temp->right;
+            else
+            temp = leftmostNode(temp->right);
+        }
+        return temp;
     }
     
 };
 
 int main()
 {
+    Tree t1(5);
+    t1.insert(3);
+    t1.insert(7);
+    t1.inorder();
     return 0;
 }
