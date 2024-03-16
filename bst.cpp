3a7a2879749daeb93a95b9feae2c04d1f36cc762
adv-AI-t <joshiadvait07@gmail.com>
2024-03-16 10:17:59 +0530

    added inorder

--- bst.cpp
+++ bst.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<stack>
 using namespace std;
 
 class Node
@@ -29,4 +30,69 @@ class Tree
     {
         root = NULL;
     }
+
+    void add(int data)
+    {
+        Node* newnode = new Node(data);
+        if(!root)
+        {
+            root = newnode;
+            return;
+        }
+        Node* ptr = root;
+        Node* parent = NULL;
+        while(ptr)
+        {
+            parent = ptr;
+            if(data<ptr->data)
+            ptr = ptr->left;
+            else
+            ptr = ptr->right;
+        }
+        if(data<parent->data)
+        parent->left = newnode;
+        else
+        parent->right = newnode;
+    }
+
+    void inorderNonrecursive()
+    {
+        stack<Node*>s1;
+        Node* temp = root;
+        while(temp || !s1.empty())
+        {
+            while(temp)
+            {
+                s1.push(temp);
+                temp = temp->left;
+            }
+            temp = s1.top();
+            cout<<temp->data<<" ";
+            s1.pop();
+            temp = temp->right;
+        }
+    }
+
+    void inorder(Node* root)
+    {
+        if(!root) return;
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+
+    void inorderhelper()
+    {
+        inorder(root);
+    }
 };
+
+int main()
+{
+    Tree t1;
+    t1.add(5);
+    t1.add(3);
+    t1.add(7);
+    t1.inorderNonrecursive();
+    return 0;
+}
